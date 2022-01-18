28a22baf16122fadcfb2b8f21423b9f5a6a3f031
Ahmed Masoud <maxzod66@gmail.com>
2022-01-18 14:10:23 +0200

    fix tests

--- lib/assets/ar.dart
+++ lib/assets/ar.dart
@@ -79,6 +79,6 @@ const arAssets = <String, Object>{
   "menu": "القائمه",
   "sort_by": "ترتيب حسب",
   "view_all": "عرض الكل",
-  "description": "الوصف",
-  "notes": "ملاحظات",
+  // "description": "الوصف",
+  // "notes": "ملاحظات",
 };
